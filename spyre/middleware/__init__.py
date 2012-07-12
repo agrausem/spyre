9f2c10ea99664c4540c3ae877c451864c0523d87
Arnaud Grausem <arnaud.grausem@gmail.com>
2012-07-12 23:26:52 +0200

    add mock middleware

--- spyre/middleware/__init__.py
+++ spyre/middleware/__init__.py
@@ -1,6 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from functools import partial
+from ..request import Request
+
+"""
+"""
 
 
-class Middleware:
-    def __init__(self):
+class Middleware(object):
+    """
+    """
+
+    def __init__(self, *args, **kwargs):
         pass
+
+
+class Mock(Middleware):
+    """
+    """
+
+    def __init__(self, fakes):
+        self.fakes = fakes
+
+    def __call__(self, env):
+        finalized_request = Request(env)
+        for path, func in self.fakes.items():
+            if path == finalized_request.path:
+                response = func(finalized_request())
+                return response if response else None
