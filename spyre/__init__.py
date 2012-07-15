9ffb02748aa575691a33be3019c2680223b9dad7
Arnaud Grausem <arnaud.grausem@gmail.com>
2012-07-15 16:00:51 +0200

    add new_from_url function

--- spyre/__init__.py
+++ spyre/__init__.py
@@ -2,6 +2,7 @@ import __future__
 from spyre.core import Spore
 from errors import *
 import os.path
+from httpclient import HTTPClient
 
 
 def new_from_spec(spec_file, base_url=None):
@@ -16,6 +17,12 @@ def new_from_string(spec_string, base_url=None):
     except Exception, e:
         raise SpyreObjectBuilder(e)
     return spore
+
+
+def new_from_url(spec_url, base_url=None):
+    http_client = HTTPClient()
+    response = http_client.get(spec_url)
+    return new_from_string(response.content, base_url)
 
 
 def _read_from_file(spec_file):
